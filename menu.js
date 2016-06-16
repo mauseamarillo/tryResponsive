fc374a04aac81e14d8667b0e47d57980b982ae88
Jhonbeltran <jbeltranleon@gmail.com>
2016-06-16 00:26:28 -0500

    base para pagina web

---
+++ menu.js
@@ -0,0 +1,24 @@
+$(document).ready(main);
+ 
+
+ 
+function main(){
+	var contador = 1;
+	$('.menu_bar').click(function(){
+		// $('nav').toggle(); 
+ 
+		if(contador == 1){
+			$('nav').animate({
+				left: '0'
+			});
+			contador = 0;
+		} else {
+			contador = 1;
+			$('nav').animate({
+				left: '-100%'
+			});
+		}
+ 
+	});
+ 
+};
